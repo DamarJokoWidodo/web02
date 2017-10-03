3b78991128b12c2fd7088e9a621b697bfa60b7ea
Damar <damarjokowi@gmail.com>
2017-10-04 03:54:43 +0700

    Delete

--- proses_dsn.php
+++ proses_dsn.php
@@ -12,7 +12,11 @@ if($_GET['action'] == "add"){
 				SET kode_dosen = '$_POST[kode_dosen]',
 					nama = '$_POST[nama]',
 				WHERE id = $_POST[id]";
+}else if($_GET['action'] == "delete"){
+	$query = "DELETE FROM dosen 
+				WHERE id=$_GET[id]";
 }
+
 mysqli_query($koneksi, $query);
 
 //Redirect ke template.php
