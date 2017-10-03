d39fc3e4e1d3eae13711ad63155062d709922c3d
Damar <damarjokowi@gmail.com>
2017-10-04 03:50:58 +0700

    Update

---
+++ proses_dsn.php
@@ -0,0 +1,19 @@
+<? //filename: proses_dsn.php 
+
+include("db.php");
+
+if($_GET['action'] == "add"){
+	//2. Query 
+	$query = "INSERT INTO dosen (kode_dosen, nama)
+			VALUES('$_POST[kode_dosen]', '$_POST[nama]')";
+}else if($_GET['action'] == 'edit'){
+	//2. Query 
+	$query = = "UPDATE dosen 
+				SET kode_dosen = '$_POST[kode_dosen]',
+					nama = '$_POST[nama]',
+				WHERE id = $_POST[id]";
+}
+mysqli_query($koneksi, $query);
+
+//Redirect ke template.php
+header('Location: template.php?page=dosen');
